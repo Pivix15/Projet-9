1284cba547396ebd1acb6d7fa7ff99d2770c2fba
Leny15 <stevynguyen@gmail.com>
2026-08-26 12:36:25 +0200

    fix(form): ajoute l'appel manquant à onSuccess après un envoi réussi

--- src/containers/Form/index.js
+++ src/containers/Form/index.js
@@ -16,6 +16,8 @@ const Form = ({ onSuccess, onError }) => {
       try {
         await mockContactApi();
         setSending(false);
+        // Afficher la confirmation du message envoyé
+        onSuccess();
       } catch (err) {
         setSending(false);
         onError(err);
